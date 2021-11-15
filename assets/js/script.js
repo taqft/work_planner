9e86e59b66f03df26605d0ed910589b6ebd40970
Bemourn <70115233+koenib@users.noreply.github.com>
2021-11-14 18:43:07 -0700

    update styles and add pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// Work planner logic
+
+
+// init global variables
+// bonus: allow days to be set as 8am, 9am, ending at diff times, etc.
+
+    // currentDay is retrieved from moment.js
+
+
+// when the page loads, display the current page
+    // and load any user's data from storage
+    // display the default layout for daily planner (8 hours)
+
+
+// color code the current, past, and future cards
+
+    // from the current hour
+    // grey-out any past cards, 
+    // highlight the current card
+
+
+// allow the text boxes to have text entered into them
+
+    // listen for a user clicking inside of a text boxes
+    // then open the text box for editing
+
+
+// add save functionality to text/card
+// bonus: warn users before leaving that there are unsaved changes
+
+    // listen for a user clicking on the lock
+
+
+// allow buttons to change color of text boxes
+
+    // listen for user clicks on buttons
+    // change the background color to the selected color
+    // store this and prepare to save
+
+
+// TODO: Research how to play haptic sounds when hovering, etc.
